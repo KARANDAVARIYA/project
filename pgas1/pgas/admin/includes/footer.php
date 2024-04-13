ee845d15a10b3dfb0df874c6ffa6abfab824bbd3
Karan Davariya <karandavariya451@gmail.com>
2024-04-13 14:01:00 +0530

    project

---
+++ pgas1/pgas/admin/includes/footer.php
@@ -0,0 +1,6 @@
+<div class="footer">
+			<div class="wthree-copyright pt-5 text-center">
+			  <p> <?php echo date("Y") ?> Paying Guest Accomodation. All rights reserved.</p>
+			  <p>Developed By <a  style="font-weight:700;padding-top:10px" href="">Mukta Patel</a></p>
+			</div>
+		  </div>
---
+++ pgas1/pgas/owner/includes/footer.php
@@ -0,0 +1,6 @@
+<div class="footer">
+			<div class="wthree-copyright text-center mt-5">
+			  <p> <?php echo date("Y") ?> Paying Guest Accomodation. All rights reserved.</p>
+			  <p>Developed By <a  style="font-weight:700;padding-top:10px" href="">Mukta Patel</a></p>
+			</div>
+		  </div>
